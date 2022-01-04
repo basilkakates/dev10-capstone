b4ccf0e603c6a2f3eaad45f9bb95be76293d580f
Funk <Jfun000>
2022-01-04 10:28:08 -0600

    added runner and user class in the models directory

---
+++ src/server/src/main/java/learn/capstone/clubrunner/models/Runner.java
@@ -0,0 +1,32 @@
+package learn.capstone.clubrunner.models;
+
+public class Runner {
+
+    private int runner_id;
+    private int run_id;
+    private int user_id;
+
+    public int getRunner_id() {
+        return runner_id;
+    }
+
+    public void setRunner_id(int runner_id) {
+        this.runner_id = runner_id;
+    }
+
+    public int getRun_id() {
+        return run_id;
+    }
+
+    public void setRun_id(int run_id) {
+        this.run_id = run_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+}
