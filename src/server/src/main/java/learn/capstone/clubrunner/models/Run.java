b95a8209755df9c549bd635a335453c08b7133be
Funk <Jfun000>
2022-01-04 10:13:43 -0600

    added run class in the new models directory

---
+++ src/server/src/main/java/learn/capstone/clubrunner/models/Run.java
@@ -0,0 +1,101 @@
+package learn.capstone.clubrunner.models;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Run {
+
+    private int run_id;
+    private LocalDate date;
+    private String address;
+    private int club_id;
+    private int user_id;
+    private int max_capacity;
+    private LocalDateTime start_time;
+    private int run_status_id;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
+
+    //Do I need to add an arraylist for matching bridge tables? like in fieldagent?
+
+    public int getRun_id() {
+        return run_id;
+    }
+
+    public void setRun_id(int run_id) {
+        this.run_id = run_id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getClub_id() {
+        return club_id;
+    }
+
+    public void setClub_id(int club_id) {
+        this.club_id = club_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getMax_capacity() {
+        return max_capacity;
+    }
+
+    public void setMax_capacity(int max_capacity) {
+        this.max_capacity = max_capacity;
+    }
+
+    public LocalDateTime getStart_time() {
+        return start_time;
+    }
+
+    public void setStart_time(LocalDateTime start_time) {
+        this.start_time = start_time;
+    }
+
+    public int getRun_status_id() {
+        return run_status_id;
+    }
+
+    public void setRun_status_id(int run_status_id) {
+        this.run_status_id = run_status_id;
+    }
+
+    public BigDecimal getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(BigDecimal latitude) {
+        this.latitude = latitude;
+    }
+
+    public BigDecimal getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(BigDecimal longitude) {
+        this.longitude = longitude;
+    }
+}
