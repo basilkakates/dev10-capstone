d7a25ee1b854cd9f2f38783ed8ca4646e9166d67
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2022-01-05 16:34:35 -0500

    Finished tests.

--- src/server/src/test/java/learn/capstone/clubrunner/data/RunStatusJdbcTemplateRepositoryTest.java
+++ src/server/src/test/java/learn/capstone/clubrunner/data/RunStatusJdbcTemplateRepositoryTest.java
@@ -2,6 +2,53 @@ package learn.capstone.clubrunner.data;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(webEnvrionment = SpringBootTest.WebEnvironment.NONE)
 class RunStatusJdbcTemplateRepositoryTest {
 
+    @Autowired
+    RunStatusJdbcTemplateRepository repository;
+
+    @Autowired
+    KnownGoodState knownGoodState;
+
+    @BeforeEach
+    void setup() { knownGoodState.set(); }
+
+    @Test
+    void shouldFindById() {
+        List<RunStatus> runStatuses = repository.findAll();
+        assertNotNull(runStatuses);
+        asserTrue(runStatuses.size() > 0);
+    }
+
+    @Test
+    void shouldFindByStatus() {
+        RunStatus testRunStatus = repository.findById(1);
+        assertEquals("", testRunStatus.getStatus());
+    }
+
+    @Test
+    void shouldAddRunStatus() {
+        RunStatus testRunStatus = new RunStatus();
+        testRunStatus.setStatus("Approved");
+        RunStatus actual = repository.add(testRunStatus);
+        assertNotNull(actual);
+        assertEquals("Approved", actual.getRunStatus());
+    }
+
+
+    @Test
+    void shouldUpdateRunStatus() {
+        RunStatus testRunStatus = new RunStatus();
+        testRunStatus.setId(1);
+        testRunStatus.setStatus("Pending");
+        assertTrue(repository.update(testRunStatus));
+        assertEquals(repository.findById(1).status, "Pending");
+    }
+
+    @Test
+    void shouldDeleteRunStatus() {
+        assertTrue(repository.deleteById(2));
+        assertFalse(repository.deleteById(2));
+    }
 }
