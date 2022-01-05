7dacc15fd2bc7b2772ce7c46e23b186be2226421
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2022-01-05 11:00:45 -0500

    Added basis for security.

--- src/client/frontend/src/App.js
+++ src/client/frontend/src/App.js
@@ -1,5 +1,13 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import jwt_decode from "jwt-decode";
 
+import AuthContext from "./AuthContext";
 import UserProfile from "./components/UserProfile";
 import Runs from "./components/Runs";
 import AddRun from "./components/AddRun";
@@ -10,49 +18,113 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Header from "./components/Header";
 import NotFound from "./components/NotFound";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
+const TOKEN_KEY = "user-api-topken";
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [initialized, setInitialized] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem(TOKEN_KEY);
+
+    if (token) {
+      login(token);
+    }
+
+    setInitialized(true);
+  }, []);
+
+  const login = (token) => {
+    const { id, sub: username, roles: userRoles } = jwt_decode(token);
+
+    const roles = userRoles?.split(",");
+
+    const user = {
+      id,
+      username,
+      roles,
+      token,
+      hasRole(role) {
+        return this.roles.includes(role);
+      },
+    };
+
+    console.log(user);
+
+    setUser(user);
+
+    return user;
+  };
+
+  const logout = () => {
+    localStorage.removeItem(TOKEN_KEY);
+    setUser(null);
+  };
+
+  const auth = {
+    user: user ? { ...user } : null,
+    login,
+    logout,
+  };
+
+  if (!initialized) {
+    return null;
+  }
+
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
+    <AuthContext.Provider value={auth}>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
 
-        <Route exact path="/about">
-          <About />
-        </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
 
-        <Route exact path="/userprofile/:user_id">
-          <UserProfile />
-        </Route>
+          <Route exact path="/userprofile/:user_id">
+            {user ? <UserProfile /> : <Redirect to="/login" />}
+          </Route>
 
-        <Route path="/runs">
-          <Runs />
-        </Route>
+          <Route path="/runs">
+            {user ? <Runs /> : <Redirect to="login" />}
+          </Route>
 
-        <Route path="/runs/add">
-          <AddRun />
-        </Route>
+          <Route path="/runs/add">
+            {user ? <AddRun /> : <Redirect to="login" />}
+          </Route>
 
-        <Route path="/runs/edit/:run_id">
-          <EditRun />
-        </Route>
+          <Route path="/runs/edit/:run_id">
+            {user ? <EditRun /> : <Redirect to="login" />}
+          </Route>
 
-        <Route path="/runs/delete/:run_id">
-          <DeleteRun />
-        </Route>
+          <Route path="/runs/delete/:run_id">
+            {user ? <DeleteRun /> : <Redirect to="login" />}
+          </Route>
 
-        <Route path="/clubs">
-          <Clubs />
-        </Route>
+          <Route path="/clubs">
+            {user ? <Clubs /> : <Redirect to="login" />}
+          </Route>
 
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
-    </Router>
+          <Route path="/login">
+            <Login />
+          </Route>
+
+          <Route path="/register">
+            <Register />
+          </Route>
+
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
--- src/client/frontend/src/components/NavBar.js
+++ src/client/frontend/src/components/NavBar.js
@@ -1,6 +1,10 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import AuthContext from "../AuthContext";
 
 function NavBar() {
+  const auth = useContext(AuthContext);
+
   return (
     <>
       <ul>
@@ -22,7 +26,25 @@ function NavBar() {
         <li>
           <Link to="/runs/pending">Pending Runs</Link>
         </li>
+        {!auth.user && (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </>
+        )}
       </ul>
+      {auth.user && (
+        <div>
+          <p>Hello {auth.user.username}!</p>
+          <button onClick={() => auth.logout()} className="btn btn-primary">
+            Logout
+          </button>
+        </div>
+      )}
     </>
   );
 }
