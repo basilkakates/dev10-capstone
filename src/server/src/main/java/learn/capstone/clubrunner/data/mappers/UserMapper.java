64c75bd72ef37884cc6468025ca89af4074637b2
Basilios Kakates <1403747+basilkakates@users.noreply.github.com>
2022-01-05 13:49:18 -0600

    Updated formatting to be consistent with team

--- src/server/src/main/java/learn/capstone/clubrunner/data/mappers/UserMapper.java
+++ src/server/src/main/java/learn/capstone/clubrunner/data/mappers/UserMapper.java
@@ -7,24 +7,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserMapper implements RowMapper<User> {
-
     @Override
     public User mapRow(ResultSet resultSet, int i) throws SQLException {
-
         User user = new User();
         user.setUser_id(resultSet.getInt("user_id"));
         user.setFirst_name(resultSet.getString("first_name"));
         user.setLast_name(resultSet.getString("last_name"));
         user.setEmail(resultSet.getString("email"));
         user.setPassword(resultSet.getString("password"));
-
-//        SecurityClearanceMapper securityClearanceMapper = new SecurityClearanceMapper();
-//        agencyAgent.setSecurityClearance(securityClearanceMapper.mapRow(resultSet, i));
-
-//          Will I need to add these as well? I think they are related to the lists.
-
-//        AgentMapper agentMapper = new AgentMapper();
-//        agencyAgent.setAgent(agentMapper.mapRow(resultSet, i));
 
         return user;
     }
