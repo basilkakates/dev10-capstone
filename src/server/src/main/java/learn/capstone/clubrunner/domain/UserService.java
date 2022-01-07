d13196f33e6766b0dc0e56482a8dd193e724828f
Basilios Kakates <1403747+basilkakates@users.noreply.github.com>
2022-01-07 13:40:15 -0600

    Create UserService and tests

---
+++ src/server/src/main/java/learn/capstone/clubrunner/domain/UserService.java
@@ -0,0 +1,139 @@
+package learn.capstone.clubrunner.domain;
+
+import learn.capstone.clubrunner.data.UserRepository;
+import learn.capstone.clubrunner.models.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public Result<User> findById(int userId) {
+        Result<User> result = new Result<>();
+        result.setPayload(repository.findById(userId));
+
+        if (result.getPayload() == null) {
+            String msg = String.format("userId: %s not found", userId);
+            result.addMessage(msg, ResultType.NOT_FOUND);
+        }
+
+        return result;
+    }
+
+    public Result<List<User>> findByName(String firstName, String lastName) {
+        Result<List<User>> result = new Result<>();
+
+        if (Validations.isNullOrBlank(firstName) || Validations.isNullOrBlank(lastName)) {
+            result.addMessage("firstName and lastName are required", ResultType.INVALID);
+        } else {
+            result.setPayload(repository.findByName(firstName, lastName));
+        }
+
+        if (result.isSuccess() && result.getPayload().size() <= 0) {
+            String msg = String.format("%s %s not found", firstName, lastName);
+            result.addMessage(msg, ResultType.NOT_FOUND);
+        }
+
+        return result;
+    }
+
+    public Result<User> findByEmail(String email) {
+        Result<User> result = new Result<>();
+
+        if (Validations.isNullOrBlank(email)) {
+            result.addMessage("email is required", ResultType.INVALID);
+            return result;
+        } else {
+            result.setPayload(repository.findByEmail(email));
+        }
+
+        if (result.getPayload() == null) {
+            String msg = String.format("email: %s not found", email);
+            result.addMessage(msg, ResultType.NOT_FOUND);
+        }
+
+        return result;
+    }
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public Result<User> add(User user) {
+        Result<User> result = validate(user);
+
+        if (!result.isSuccess()) {
+            return result;
+        }
+
+        if (user.getUserId() != 0) {
+            result.addMessage("userId cannot be set for `add` operation", ResultType.INVALID);
+        } else if (repository.findByEmail(user.getEmail()) != null) {
+            result.addMessage("duplicate emails are not permitted", ResultType.INVALID);
+        } else {
+            result.setPayload(repository.add(user));
+        }
+
+        return result;
+    }
+
+    public Result<User> update(User user) {
+        Result<User> result = validate(user);
+
+        if (!result.isSuccess()) {
+            return result;
+        }
+
+        if (user.getUserId() <= 0) {
+            result.addMessage("userID must be set for `update` operation", ResultType.INVALID);
+            return result;
+        }
+
+        User userFindByEmail = repository.findByEmail(user.getEmail());
+
+        if (userFindByEmail != null && userFindByEmail.getUserId() != user.getUserId()) {
+            result.addMessage("duplicate emails are not permitted", ResultType.INVALID);
+            return result;
+        }
+
+        if (!repository.update(user)) {
+            String msg = String.format("userId: %s, not found", user.getUserId());
+            result.addMessage(msg, ResultType.NOT_FOUND);
+        }
+
+        return result;
+    }
+
+    private Result<User> validate(User user) {
+        Result<User> result = new Result<>();
+
+        if (user == null) {
+            result.addMessage("user cannot be null", ResultType.INVALID);
+            return result;
+        }
+
+        if (Validations.isNullOrBlank(user.getFirstName())) {
+            result.addMessage("firstName is required", ResultType.INVALID);
+        }
+
+        if (Validations.isNullOrBlank(user.getLastName())) {
+            result.addMessage("lastName is required", ResultType.INVALID);
+        }
+
+        if (Validations.isNullOrBlank(user.getEmail())) {
+            result.addMessage("email is required", ResultType.INVALID);
+        }
+
+        if (Validations.isNullOrBlank(user.getPassword())) {
+            result.addMessage("password is required", ResultType.INVALID);
+        }
+
+        return result;
+    }
+}
---
+++ src/server/src/test/java/learn/capstone/clubrunner/domain/UserServiceTest.java
@@ -0,0 +1,328 @@
+package learn.capstone.clubrunner.domain;
+
+import learn.capstone.clubrunner.data.UserRepository;
+import learn.capstone.clubrunner.models.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+class UserServiceTest {
+    @Autowired
+    UserService service;
+
+    @MockBean
+    UserRepository repository;
+
+    @Test
+    void shouldNotFindMissingId() {
+        when(repository.findById(100000)).thenReturn(null);
+
+        Result<User> actual = service.findById(100000);
+        assertNotNull(actual);
+        assertEquals(ResultType.NOT_FOUND, actual.getType());
+        assertNull(actual.getPayload());
+    }
+
+    @Test
+    void shouldFindById() {
+        User user = makeUser();
+        user.setUserId(1);
+        when(repository.findById(user.getUserId())).thenReturn(user);
+
+        Result<User> actual = service.findById(user.getUserId());
+        assertNotNull(actual);
+        assertEquals(ResultType.SUCCESS, actual.getType());
+        assertNotNull(actual.getPayload());
+        assertEquals(user, actual.getPayload());
+    }
+
+    @Test
+    void shouldNotFindByNullName() {
+        Result<List<User>> actualFirstName = service.findByName(null, "McTesty");
+        Result<List<User>> actualLastName = service.findByName("Testy", null);
+
+        assertNotNull(actualFirstName);
+        assertEquals(ResultType.INVALID, actualFirstName.getType());
+        assertNull(actualFirstName.getPayload());
+
+        assertNotNull(actualLastName);
+        assertEquals(ResultType.INVALID, actualLastName.getType());
+        assertNull(actualLastName.getPayload());
+    }
+
+    @Test
+    void shouldNotFindByMissingName() {
+        List<User> expected = new ArrayList<>();
+        User user = makeUser();
+
+        when(repository.findByName(user.getFirstName(), user.getLastName())).thenReturn(expected);
+
+        Result<List<User>> actual = service.findByName(user.getFirstName(), user.getLastName());
+        assertNotNull(actual);
+        assertEquals(ResultType.NOT_FOUND, actual.getType());
+        assertNotNull(actual);
+        assertEquals(0, actual.getPayload().size());
+    }
+
+    @Test
+    void shouldFindByName() {
+        User user = makeUser();
+        user.setUserId(1);
+        when(repository.findByName(user.getFirstName(), user.getLastName())).thenReturn(List.of(user));
+
+        Result<List<User>> actual = service.findByName(user.getFirstName(), user.getLastName());
+        assertNotNull(actual);
+        assertEquals(ResultType.SUCCESS, actual.getType());
+        assertNotNull(actual.getPayload());
+        assertEquals(user, actual.getPayload().get(0));
+    }
+
+    @Test
+    void shouldNotFindByNullEmail() {
+        Result<User> actual = service.findByEmail(null);
+        assertNotNull(actual);
+        assertEquals(ResultType.INVALID, actual.getType());
+        assertNull(actual.getPayload());
+    }
+
+    @Test
+    void shouldNotFindByMissingEmail() {
+        User user = makeUser();
+        user.setUserId(1);
+        when(repository.findByEmail(user.getEmail())).thenReturn(null);
+
+        Result<User> actual = service.findByEmail(user.getEmail());
+        assertNotNull(actual);
+        assertEquals(ResultType.NOT_FOUND, actual.getType());
+        assertNull(actual.getPayload());
+    }
+
+    @Test
+    void shouldFindByEmail() {
+        User user = makeUser();
+        user.setUserId(1);
+        when(repository.findByEmail(user.getEmail())).thenReturn(user);
+
+        Result<User> actual = service.findByEmail(user.getEmail());
+        assertNotNull(actual);
+        assertEquals(ResultType.SUCCESS, actual.getType());
+        assertNotNull(actual.getPayload());
+        assertEquals(user, actual.getPayload());
+    }
+
+    @Test
+    void shouldNotAddNullUser() {
+        Result<User> actual = service.add(null);
+        assertNotNull(actual);
+        assertEquals(ResultType.INVALID, actual.getType());
+        assertNull(actual.getPayload());
+    }
+
+    @Test
+    void shouldNotAddNullFirstName() {
+        User user = makeUser();
+        user.setUserId(1);
+        user.setFirstName(null);
+
+        Result<User> actual = service.add(user);
+        assertNotNull(actual);
+        assertEquals(ResultType.INVALID, actual.getType());
+        assertNull(actual.getPayload());
+    }
+
+    @Test
+    void shouldNotAddNullLastName() {
+        User user = makeUser();
+        user.setUserId(1);
+        user.setLastName(null);
+
+        Result<User> actual = service.add(user);
+        assertNotNull(actual);
+        assertEquals(ResultType.INVALID, actual.getType());
+        assertNull(actual.getPayload());
+    }
+
+    @Test
+    void shouldNotAddNullEmail() {
+        User user = makeUser();
+        user.setUserId(1);
+        user.setEmail(null);
+
+        Result<User> actual = service.add(user);
+        assertNotNull(actual);
+        assertEquals(ResultType.INVALID, actual.getType());
+        assertNull(actual.getPayload());
+    }
+
+    @Test
+    void shouldNotAddNullPassword() {
+        User user = makeUser();
+        user.setUserId(1);
+        user.setPassword(null);
+
+        Result<User> actual = service.add(user);
+        assertNotNull(actual);
+        assertEquals(ResultType.INVALID, actual.getType());
+        assertNull(actual.getPayload());
+    }
+
+    @Test
+    void shouldNotAddWithUserId() {
+        User user = makeUser();
+        user.setUserId(1);
+
+        Result<User> actual = service.add(user);
+        assertNotNull(actual);
+        assertEquals(ResultType.INVALID, actual.getType());
+        assertNull(actual.getPayload());
+    }
+
+    @Test
+    void shouldNotAddDuplicateEmail() {
+        User user = makeUser();
+        when(repository.findByEmail(user.getEmail())).thenReturn(user);
+
+        Result<User> actual = service.add(user);
+        assertNotNull(actual);
+        assertEquals(ResultType.INVALID, actual.getType());
+        assertNull(actual.getPayload());
+    }
+
+    @Test
+    void shouldAddUser() {
+        User expected = makeUser();
+        expected.setUserId(1);
+
+        when(repository.findByEmail(expected.getEmail())).thenReturn(null);
+        when(repository.add(makeUser())).thenReturn(expected);
+
+        Result<User> actual = service.add(makeUser());
+        assertNotNull(actual);
+        assertEquals(ResultType.SUCCESS, actual.getType());
+        assertNotNull(actual.getPayload());
+        assertEquals(expected, actual.getPayload());
+    }
+
+    @Test
+    void shouldNotUpdateNullUser() {
+        Result<User> actual = service.update(null);
+        assertNotNull(actual);
+        assertEquals(ResultType.INVALID, actual.getType());
+        assertNull(actual.getPayload());
+    }
+
+    @Test
+    void shouldNotUpdateNullFirstName() {
+        User user = makeUser();
+        user.setUserId(1);
+        user.setFirstName(null);
+
+        Result<User> actual = service.update(user);
+        assertNotNull(actual);
+        assertEquals(ResultType.INVALID, actual.getType());
+        assertNull(actual.getPayload());
+    }
+
+    @Test
+    void shouldNotUpdateNullLastName() {
+        User user = makeUser();
+        user.setUserId(1);
+        user.setLastName(null);
+
+        Result<User> actual = service.update(user);
+        assertNotNull(actual);
+        assertEquals(ResultType.INVALID, actual.getType());
+        assertNull(actual.getPayload());
+    }
+
+    @Test
+    void shouldNotUpdateNullEmail() {
+        User user = makeUser();
+        user.setUserId(1);
+        user.setEmail(null);
+
+        Result<User> actual = service.update(user);
+        assertNotNull(actual);
+        assertEquals(ResultType.INVALID, actual.getType());
+        assertNull(actual.getPayload());
+    }
+
+    @Test
+    void shouldNotUpdateNullPassword() {
+        User user = makeUser();
+        user.setUserId(1);
+        user.setPassword(null);
+
+        Result<User> actual = service.update(user);
+        assertNotNull(actual);
+        assertEquals(ResultType.INVALID, actual.getType());
+        assertNull(actual.getPayload());
+    }
+
+    @Test
+    void shouldNotUpdateMissingId() {
+        Result<User> actual = service.update(makeUser());
+        assertNotNull(actual);
+        assertEquals(ResultType.INVALID, actual.getType());
+        assertNull(actual.getPayload());
+    }
+
+    @Test
+    void shouldNotUpdateWithDuplicateEmail() {
+        User user = makeUser();
+        user.setUserId(1);
+
+        when(repository.findByEmail(user.getEmail())).thenReturn(makeUser());
+        Result<User> actual = service.update(user);
+        assertNotNull(actual);
+        assertEquals(ResultType.INVALID, actual.getType());
+        assertNull(actual.getPayload());
+    }
+
+    @Test
+    void shouldNotUpdateMissingUser() {
+        User user = makeUser();
+        user.setUserId(100000);
+
+        when(repository.findByEmail(user.getEmail())).thenReturn(null);
+        when(repository.update(user)).thenReturn(false);
+
+        Result<User> actual = service.update(user);
+        assertNotNull(actual);
+        assertEquals(ResultType.NOT_FOUND, actual.getType());
+        assertNull(actual.getPayload());
+    }
+
+    @Test
+    void shouldUpdateUser() {
+        User user = makeUser();
+        user.setUserId(1);
+
+        when(repository.findByEmail(user.getEmail())).thenReturn(user);
+        when(repository.update(user)).thenReturn(true);
+
+        Result<User> actual = service.update(user);
+        assertNotNull(actual);
+        assertEquals(ResultType.SUCCESS, actual.getType());
+        assertNull(actual.getPayload());
+    }
+
+    private User makeUser() {
+        User user = new User();
+        user.setUserId(0);
+        user.setFirstName("Testy");
+        user.setLastName("McTest");
+        user.setEmail("tmctesty@test.com");
+        user.setPassword("supersecure");
+        return user;
+    }
+
+}
