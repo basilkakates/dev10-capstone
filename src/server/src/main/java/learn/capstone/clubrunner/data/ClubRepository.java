03fe53edd21c42ebb92c074845ccfda8565107bf
Basilios Kakates <1403747+basilkakates@users.noreply.github.com>
2022-01-09 15:17:36 -0600

    Added getMethods
    added getMethods findAdminFor and findClubMemberships

--- src/server/src/main/java/learn/capstone/clubrunner/data/ClubRepository.java
+++ src/server/src/main/java/learn/capstone/clubrunner/data/ClubRepository.java
@@ -1,13 +1,15 @@
 package learn.capstone.clubrunner.data;
 
 import learn.capstone.clubrunner.models.Club;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface ClubRepository {
     List<Club> findAll();
 
-    @Transactional
     Club findById(int clubId);
+
+    Club findAdminFor(int userId);
+
+    List<Club> findClubMemberships(int userId);
 }
