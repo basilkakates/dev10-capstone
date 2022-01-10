2846cd4a53bbcd7a53055c497fff5debc1340fed
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2022-01-10 15:59:08 -0500

    Pending runs show for admin kind of

--- src/client/frontend/src/components/PendingRuns.js
+++ src/client/frontend/src/components/PendingRuns.js
@@ -1,22 +1,11 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
-import Button from "react-bootstrap/Button";
 
 import RunTableHeader from "./RunTableHeader";
-import ApproveRun from "./ApproveRun";
-import DeleteRun from "./DeleteRun";
+import ShowPendingRun from "./ShowPendingRun";
 
 function PendingRuns() {
   const [runs, setRuns] = useState([]);
-  const [showApproveModal, setShowApproveModal] = useState(false);
-  const [showDeclineModal, setShowDeclineModal] = useState(false);
-
-  const handleApproveModalClose = () => setShowApproveModal(false);
-  const handleApproveModalShow = () => setShowApproveModal(true);
-
-  const handleDeclineModalClose = () => setShowDeclineModal(false);
-  const handleDeclineModalShow = () => setShowDeclineModal(true);
 
   const getRuns = () => {
     fetch("http://localhost:8080/api/run")
@@ -34,24 +23,6 @@ function PendingRuns() {
     getRuns();
   }, []);
 
-  const runDeleteClickHandler = (runId) => {
-    const init = {
-      method: "DELETE",
-    };
-
-    fetch(`http://localhost:8080/api/run/${runId}`, init)
-      .then((response) => {
-        if (response.status === 204) {
-          getRuns();
-        } else if (response.status === 404) {
-          Promise.reject(`Run ID ${runId} not found`);
-        } else {
-          Promise.reject("Something unexpected went wrong :)");
-        }
-      })
-      .catch((error) => console.log(error));
-  };
-
   return (
     <Container>
       <h2 className="my-4">Pending Runs</h2>
@@ -62,47 +33,10 @@ function PendingRuns() {
         </thead>
         <tbody>
           {runs.map((run) => (
-            <tr key={run.runId}>
-              {run.status === "pending" && (
-                <>
-                  <th scrope="row">{run.date}</th>
-                  <td>{run.startTime}</td>
-                  <td>{run.address}</td>
-                  <td>{run.description}</td>
-                  <td>{run.clubid}</td>
-                  <td>{run.maxCapacity}</td>
-                  <td>
-                    <div>
-                      <Button
-                        variant="primary"
-                        onClick={handleApproveModalShow}
-                      >
-                        Approve
-                      </Button>
-                      <ApproveRun
-                        showModal={showApproveModal}
-                        closeModal={handleApproveModalClose}
-                        runId={run.runId}
-                      />
-                    </div>
-                    <div>
-                      <Button
-                        variant="secondary"
-                        onClick={handleDeclineModalShow}
-                      >
-                        Decline
-                      </Button>
-                      <DeleteRun
-                        showModal={showDeclineModal}
-                        closeModal={handleDeclineModalClose}
-                        runId={run.runId}
-                      />
-                    </div>
-                  </td>
-                </>
-              )}
-            </tr>
-          ))}
+                <ShowPendingRun run={run} />
+              )
+
+          )}
         </tbody>
       </table>
     </Container>
