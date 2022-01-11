cba37d3a3c943460f52f05c8643763ea7df97783
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2022-01-11 14:54:50 -0500

    Disabled login features

--- src/client/frontend/src/components/Home.js
+++ src/client/frontend/src/components/Home.js
@@ -1,13 +1,27 @@
 import Container from "react-bootstrap/Container";
 import Runs from "./Runs";
+import { useState } from "react";
+import PlacesAutocomplete from "./PlacesAutocomplete";
 
 function Home() {
+  const [address, setAddress] = useState("");
+  const addressOnChangeHandler = (event) => {
+    setAddress(event.target.value);
+  };
   return (
     <Container>
       <h2 className="my-4">
         Welcome to Club Runner. Please Login or Register to sign up for runs.
       </h2>
-      <Runs />
+      {/* <Runs /> */}
+      <PlacesAutocomplete />
+      <input
+        type="text"
+        id="address"
+        name="address"
+        value={address}
+        onChange={addressOnChangeHandler}
+      />
     </Container>
   );
 }
