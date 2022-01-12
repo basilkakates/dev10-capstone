41fe33741519860093c41dd5d3892b1c8b5c3afe
Basilios Kakates <1403747+basilkakates@users.noreply.github.com>
2022-01-11 22:37:45 -0600

    Updated Database
    Updated Database to use timestamp instead of Time and Date

--- src/server/sql/club-runner-schema-prod.sql
+++ src/server/sql/club-runner-schema-prod.sql
@@ -65,11 +65,10 @@ DROP TABLE IF EXISTS `club-runner`.`run` ;
 
 CREATE TABLE IF NOT EXISTS `club-runner`.`run` (
   `run_id` INT NOT NULL AUTO_INCREMENT,
-  `date` DATE NOT NULL,
+  `timestamp` TIMESTAMP NOT NULL,
   `address` VARCHAR(45) NOT NULL,
   `description` VARCHAR(45) NULL,
   `max_capacity` INT NULL,
-  `start_time` TIME NOT NULL,
   `latitude` DECIMAL(8,6) NOT NULL,
   `longitude` DECIMAL(9,6) NOT NULL,
   `user_id` INT NOT NULL,
