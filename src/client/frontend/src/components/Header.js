db31ddc7dff9fc90106f880e6f1763f0c8b6d0ec
Drew Lindstrom <75282078+drew-lindstrom@users.noreply.github.com>
2022-01-13 13:04:24 -0500

    Removed additional login/register button

--- src/client/frontend/src/components/Header.js
+++ src/client/frontend/src/components/Header.js
@@ -1,10 +1,14 @@
+import { useContext } from "react";
 import NavBar from "./NavBar";
 import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import AuthContext from "../AuthContext";
 
 function Header() {
+  const auth = useContext(AuthContext);
+
   return (
     <>
       <Container>
@@ -13,20 +17,29 @@ function Header() {
             <h1 className="my-2">Club Runner</h1>
           </Col>
           <Col class="col-md-2 offset-md-6 align-self-center">
-            <Link to="/login" className="btn btn-primary">
-              Login
-            </Link>
-            <Link to="/register" className="btn btn-secondary">
-              Register
-            </Link>
+            {!auth.user && (
+              <>
+                <Link to="/login" className="btn btn-primary">
+                  Login
+                </Link>
+                <Link to="/register" className="btn btn-secondary">
+                  Register
+                </Link>
+              </>
+            )}
+            {auth.user && (
+              <>
+                <button
+                  onClick={() => auth.logout()}
+                  className="btn btn-primary"
+                >
+                  Logout
+                </button>
+              </>
+            )}
           </Col>
         </Row>
       </Container>
-
-      <div class="col">
-        <h2> </h2>
-      </div>
-
       <NavBar />
     </>
   );
