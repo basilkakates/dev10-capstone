01a5f5fe2e99d8dafbfd2b2cbc12745c5bb85f35
JFun000 <91906336+JFun000@users.noreply.github.com>
2022-01-13 16:06:29 -0600

    changed styling of header login and register

--- src/client/frontend/src/components/Header.js
+++ src/client/frontend/src/components/Header.js
@@ -14,7 +14,7 @@ function Header() {
       <Container>
         <Row className="row justify-content-between">
           <Col class="col-4 align-self-center">
-            <h1 className="my-2">Club Runner</h1>
+            <h1 className="my-2"style={{color: "white", background: "#375d83", width:"255px", height: "55px", fontWeight: "500", borderRadius: "10px"}}>&nbsp;Club Runner</h1>
           </Col>
           <Col class="col-md-2 offset-md-6 align-self-center">
             {!auth.user && (
